3cd44fe1d21d06645323bdb72742b339ebd376ce
Kelechi Orunta <kelechiorunta1@gmail.com>
2026-06-26 23:49:44 +0100

    feat(graphql): implement getProducts schema.ts

--- graphql/schema/schema.ts
+++ graphql/schema/schema.ts
@@ -1,4 +1,6 @@
-import { GraphQLSchema, GraphQLObjectType, GraphQLString } from 'graphql';
+import { GraphQLSchema, GraphQLObjectType, GraphQLString, GraphQLList } from 'graphql';
+import { ProductType } from '../types/types';
+import axios from 'axios';
 
 /**
  * Construct a GraphQL schema and define the necessary resolvers.
@@ -15,8 +17,23 @@ const schema = new GraphQLSchema({
         type: GraphQLString,
         resolve: () => 'world',
       },
+
+      getProducts: {
+      type: new GraphQLList(ProductType),
+      resolve: async (_: any, __: any, { context }: {context: any}) => {
+        try {
+          const products = await axios.get('http://localhost:3980/api/products');
+          return products ? products.data : [];
+        } catch (error) {
+          console.error(error instanceof Error ? error.message : error);
+        }
+      }
     },
+    },
+
+    
   }),
+  
 });
 
 export default schema
